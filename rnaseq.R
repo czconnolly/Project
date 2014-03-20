681b9c326fd9cedf15c4a1148234b2f509660cd7
czconnolly <c.z.connolly@dundee.ac.uk>
2014-03-20 14:57:02 +0000

    initial commit

---
+++ rnaseq.R
@@ -0,0 +1,40 @@
+require(DESeq2)
+require(RCurl)
+require(biomaRt)
+
+remoteFile <- getURL(paste0("http://www.compbio.dundee.ac.uk/",
+														 "user/pschofield/Teaching/Bioinformatics/",
+														 "Data/RNAseqCounts.txt"))
+
+geneCounts<-read.delim(textConnection(remoteFile),
+											 head=T,sep="\t",skip=1,row.names=1)
+
+nonZeroCounts<-geneCounts[rowSums(geneCounts[,6:11])>0,6:11]
+
+treatments <- as.factor(substr(colnames(nonZeroCounts),1,1))
+
+dds <- DESeqDataSetFromMatrix(as.matrix(nonZeroCounts),
+															as.data.frame(treatments),
+															design=~treatments)
+
+dds$treatments <- relevel(dds$treatments,"U")
+
+dds <- DESeq(dds)
+
+DEresults <- results(dds)
+
+# get annotations
+mart <- useMart(biomart="ENSEMBL_MART_ENSEMBL", 
+								host="www.ensembl.org", 
+								path="/biomart/martservice")
+
+hg19 <- useDataset("hsapiens_gene_ensembl",mart=mart)
+
+annot <- getBM(attributes=c("ensembl_gene_id",
+														"external_gene_id",
+														"affy_hg_u133b"),
+							 filter="ensembl_gene_id",
+							 values=rownames(DEresults),
+							 mart=hg19)
+
+annotResults <- merge(annot,DEresults,by.x="ensembl_gene_id",by.y="row.names")
