41a8bf4957944aca2861027299b3c8d9b1b7ae48
czconnolly <c.z.connolly@dundee.ac.uk>
2014-03-22 18:06:42 +0000

    gets stuck at fitting model and testing

--- rnaseq.R
+++ rnaseq.R
@@ -1,52 +1,31 @@
 require(DESeq2)
 require(RCurl)
 require(biomaRt)
-
+require(stringr)
 
 
 dat<-read.delim('/homes/czconnolly/BS32010/Projectgit/RNAseqCounts.txt',
                 sep='\t',skip=1,head=T)
 
-#remoteFile <- getURL(paste0("http://www.compbio.dundee.ac.uk/",
-#														 "user/pschofield/Teaching/Bioinformatics/",
-#														 "Data/RNAseqCounts.txt"))
-
-#geneCounts<-read.delim(textConnection(remoteFile),
-#											 head=T,sep="\t",skip=1,row.names=1)
-
 nonZeroCounts<-dat[rowSums(dat[,6:11])>0,6:11]
 
-treatments <- as.factor(substr(colnames(nonZeroCounts),1,1))
+treatments <- as.factor(substr(colnames(nonZeroCounts),55,65))
 
 dds <- DESeqDataSetFromMatrix(as.matrix(nonZeroCounts),
 															as.data.frame(treatments),
 															design=~treatments)
 
-dds$treatments <- relevel(dds$treatments,"U")
+dds$treatments <- relevel(dds$treatments,"DRB1_minus.")
 
 dds <- DESeq(dds)
 
 DEresults <- results(dds)
 
-# get annotations
-#mart <- useMart(biomart="ENSEMBL_MART_ENSEMBL", 
-#								host="www.ensembl.org", 
-#								path="/biomart/martservice")
-
 mart<-useMart(biomart='ENSEMBL_MART_ENSEMBL',
               host='www.ensemble.org',
               path='/biomart/martservice')
 
-#hg19 <- useDataset("hsapiens_gene_ensembl",mart=mart)
-
 gg4<-useDataset('ggallus_gene_ensemble',mart=mart)
-
-#annot <- getBM(attributes=c("ensembl_gene_id",
-#														"external_gene_id",
-#														"affy_hg_u133b"),
-#							 filter="ensembl_gene_id",
-#							 values=rownames(DEresults),
-#							 mart=hg19)
 
 annot<-getBM(attributes=c('ensemble_gene_id',
                           'external_gene_id',
