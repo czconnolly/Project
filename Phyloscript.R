2ff3999af53ec7ca882eebf4e324753459684f59
czconnolly <c.z.connolly@dundee.ac.uk>
2014-03-30 18:53:08 +0100

    Update Phyloscript.R

--- Phyloscript.R
+++ Phyloscript.R
@@ -1,12 +1,19 @@
+#Script to plot phylogenetic trees, create distance matrices, calculate regression using ape package in R
+
+#read in alignment
 x<-read.dna('SLC45A3_musclealignment.mfa', format='fasta')
+#carry out model test on alignment
 mtSLC45A3<-modelTest(as.phyDat(x),G=F,I=F)
+#calculates DNA distances
 d<-dist.dna(x)
 
+#creates trees using the algorithms
 tr.upgmaSLC45A3<-upgma(d)
 tr.njSLC45A3<-nj(d)
 tr.bionjSLC45A3<-bionj(d)
 tr.fastmeSLC45A3<-fastme.bal(d,nni=T,spr=T,tbr=T)
 
+#calculates regression
 dt.upgmaSLC45A3<-cophenetic(tr.upgmaSLC45A3)
 dmatupgma<-as.matrix(d)
 nmsupgma<-rownames(dmatupgma)
@@ -31,6 +38,7 @@ nmsfastme<-rownames(dmatfastme)
 dt.fastmeSLC45A3<-dt.fastmeSLC45A3[nmsfastme,nmsfastme]
 dt.fastmeSLC45A3<-as.dist(dt.fastmeSLC45A3)
 
+#plots the trees
 par(mfrow=c(2,2))
 plot(tr.upgmaSLC45A3, main='UPGMA')
 plot(tr.fastmeSLC45A3, main='FASTME')
